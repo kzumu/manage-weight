1febee989515bedd15da838d8e6382532b4b7747
Kazumasa Shimomura <kazlv.1110@gmail.com>
2017-09-26 14:11:46 +0900

    get field value

--- app/src/main/java/com/kaz/manageweight/MainActivity.java
+++ app/src/main/java/com/kaz/manageweight/MainActivity.java
@@ -1,7 +1,11 @@
 package com.kaz.manageweight;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textview = (TextView) findViewById(R.id.textview);
+        textview.setText("書き換えたよ");
+
+        setup();
+    }
+
+    protected void setup() {
+        findViewById(R.id.registerButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("", "-----Clicked-----");
+                EditText bwField = (EditText)findViewById(R.id.bwField);
+                EditText bfField = (EditText) findViewById(R.id.bfField);
+                Log.d("", String.format("BW: %s, BF: %s", bwField.getText() , bfField.getText() ));
+            }
+        });
     }
 }
